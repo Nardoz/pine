d5605628153b4ca44c4062659f7fab8d30437428
Nicolás Melendez <nfmelendez@gmail.com>
2014-09-20 19:35:55 -0300

    add static path

--- webapi/app-server.js
+++ webapi/app-server.js
@@ -3,9 +3,13 @@ var express = require('express'),
 	port = 8001,
 	tweetController = require('./src/controllers/tweet').tweetController;
 
-app.get('/:uid', tweetController.getByUserId);
+app.get('/api/:uid', tweetController.getByUserId);
 
-app.get('/:uid/:tid', tweetController.getByTweetId);
+app.get('api/:uid/:tid', tweetController.getByTweetId);
+
+app.use(express.static('../ui'));
+
+
 
 console.log('Starting server on port: ' + port);
 app.listen(port);
