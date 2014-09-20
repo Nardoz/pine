1aa8c77a7abb7db0f7783b55c4618223e1dc6f68
ezequiel <ezequiel.golub@gmail.com>
2014-09-20 19:07:27 -0300

    Trabajo duro, como un esclavo

--- ui/js/main.js
+++ ui/js/main.js
@@ -1,16 +1,49 @@
-var app = $.sammy(function() {
-    // include the plugin and alias mustache() to ms()
-    this.use('Mustache', 'ms');
-    
-    this.get('/', function() {
-      // set local vars
-      this.title = 'Hello!'
-      this.name = "eze"
-      // render the template and pass it through mustache
-      this.partial('templates/index.ms');
-    });
-  });
+function get_user_or_tweet(text) { 
+	if (text.match(/^(\w+|_*)$/) && text.length <= 15) { 
+		return {screen_name: text};
+	} else { 
+		var match = text.match(/^https:\/\/twitter\.com\/(.+?)\/status\/(.+)$/)
+		if (match) { 
+			return {screen_name: match[1], tweet_id: match[2]};
+		}
+	}
+}
 
-  $(function() {
-    app.run()
-  });
+var app = $.sammy(function() {
+	 this.debug = true;
+	// include the plugin and alias mustache() to ms()
+	this.use('Mustache', 'ms');
+
+	this.get('/', function(context) {
+		this.load('templates/index.ms').then(function(partial) { 
+			context.partials = {contents:partial};
+			// render the template and pass it through mustache
+			context.partial('templates/template.ms');
+		});
+	});
+
+	this.post('#/search', function(context) {
+		var params = get_user_or_tweet(this.params.query);
+		console.log(params);
+		this.load('templates/graph.ms').then(function(partial) { 
+			context.partials = {contents:partial};
+			// render the template and pass it through mustache
+			context.partial('templates/template.ms');
+		});
+	});
+	this.post('#/search', function(context) {
+		var params = get_user_or_tweet(this.params.query);
+		console.log(params);
+		this.load('templates/graph.ms').then(function(partial) { 
+			context.partials = {contents:partial};
+			// render the template and pass it through mustache
+			context.partial('templates/template.ms');
+		});
+	});
+
+
+});
+
+$(function() {
+	app.run()
+});
